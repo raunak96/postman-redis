2769f13098290bbce8810a3ce34c593039bd49f3
Raunak Kedia <raunakkedia1@gmail.com>
2021-07-03 16:27:11 +0530

    removed small bug

--- components/JsonEditor.jsx
+++ components/JsonEditor.jsx
@@ -1,26 +1,22 @@
 import Editor from "@monaco-editor/react";
 import { forwardRef } from "react";
 
-const JsonEditor = forwardRef(
-	(
-		{ defaultValue = `{\n\t\n}`, className, readOnly = false, height },
-		ref
-	) => {
-		const handleEditorDidMount = editor => (ref.current = editor);
-		return (
-			<>
-				<Editor
-					language="json"
-					onMount={handleEditorDidMount}
-					theme="light"
-					className={`border border-gray-300 shadow-md rounded ${className}`}
-					options={{ readOnly }}
-					height={height}
-					value={defaultValue}
-				/>
-			</>
-		);
-	}
-);
+const JsonEditor = (
+	{ defaultValue = `{\n\t\n}`, className, readOnly = false, height },
+	ref
+) => {
+	const handleEditorDidMount = editor => (ref.current = editor);
+	return (
+		<Editor
+			language="json"
+			onMount={handleEditorDidMount}
+			theme="light"
+			className={`border border-gray-300 shadow-md rounded ${className}`}
+			options={{ readOnly }}
+			height={height}
+			value={defaultValue}
+		/>
+	);
+};
 
-export default JsonEditor;
+export default forwardRef(JsonEditor);
